9083a6dfe76588c469828286780efade7fa58ae3
ntkien2907 <j13daian@gmail.com>
2021-02-02 17:57:15 +0700

    Create skeleton

---
+++ real_estate_web/HomePage/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+STATUS = (('FOR SALE', 'For Sale'), ('FOR RENT', 'For Rent'))
+
+class Post(models.Model):
+    status = models.CharField(max_length=10, choices=STATUS)
+    price = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    # create foreign key from post to user
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
---
+++ real_estate_web/HomePage/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'HomePage/home.html', context)
+
+
+def about(request):
+    return render(request, 'HomePage/about.html', {'title': 'About'})
+
+    
